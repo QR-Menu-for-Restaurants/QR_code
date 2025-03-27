b42f256c872d02304f3ff0751c64dfd61189bdcb
Hojiakbar <hojiakbar.offical28@gmail.com>
2025-03-27 14:28:13 +0500

    ejs updated

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -5,11 +5,12 @@ function openEditModal(id, name, description, imageUrl, price, category) {
     document.getElementById("editImage").value = imageUrl;
     document.getElementById("editPrice").value = price;
     document.getElementById("editCategory").value = category;
-
+  
     document.getElementById("editForm").action = `/foods/update/${id}`;
     document.getElementById("editModal").style.display = "flex";
-}
-
-function closeModal() {
+  }
+  
+  function closeModal() {
     document.getElementById("editModal").style.display = "none";
-}
+  }
+  
