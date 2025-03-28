f9a89d5bcb2c7ec8db5e844bf6491e49aaac5dd5
21Savage <tursunboyevx43@gmail.com>
2025-03-28 15:45:14 +0500

    skaner qilish yaxshilndi

--- src/controller/qrcode.controller.js
+++ src/controller/qrcode.controller.js
@@ -1,9 +1,23 @@
 import QRCode from 'qrcode';
+import os from 'os';
 import { PORT } from '../config/app.config.js';
+
+const getLocalIp = () => {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const net of interfaces[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return 'localhost';
+};
 
 export const generateQR = async (req, res) => {
     try {
-        const qrUrl = `http://localhost:${PORT}/register`; 
+        const localIp = getLocalIp();
+        const qrUrl = `http://${localIp}:${PORT}/register`; 
         const qrImage = await QRCode.toDataURL(qrUrl);
 
         res.render("qrcode", { qrImage, qrUrl }); 
