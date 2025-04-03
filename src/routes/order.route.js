9cbb0ba57072b046f95229f3bd82adb677889ce5
21Savage <tursunboyevx43@gmail.com>
2025-04-03 11:34:22 +0500

    a little bit change

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -6,6 +6,7 @@ import orderController from "../controller/order.controller.js";
 const orderRouter = Router();
 
 orderRouter.post('/', ValidationMiddleware(createOrderSchema), orderController.createOrder);
+orderRouter.get('/', orderController.getOrders);
 orderRouter.get('/:id', orderController.getOrderById);
 orderRouter.put('/:id', ValidationMiddleware(updateOrderSchema),orderController.updateOrder);
 orderRouter.put('/:orderId/foods', ValidationMiddleware(updateOrderStatusSchema), orderController.updateOrderStatus);
